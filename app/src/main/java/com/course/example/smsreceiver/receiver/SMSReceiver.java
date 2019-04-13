9c53cc0457ecaecc314d8d6a995edd9aeb6d73b2
ClassroomAccount <jpepe44@gmail.com>
2019-04-13 10:13:02 -0400

    update rev

--- app/src/main/java/com/course/example/smsreceiver/receiver/SMSReceiver.java
+++ app/src/main/java/com/course/example/smsreceiver/receiver/SMSReceiver.java
@@ -15,18 +15,29 @@ import android.widget.Toast;
 import android.util.Log;
 
 public class SMSReceiver extends BroadcastReceiver {
-    
+
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+    private static final String TAG = "SMSBroadcastReceiver";
+    private SmsMessage[] smsMessage;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Bundle bundle = intent.getExtras();
+        Log.i(TAG, "Intent recieved: " + intent.getAction());
 
         //read messages from hardware into an array
-        Object[] messages = (Object[]) bundle.get("pdus");
-        SmsMessage smsMessage[] = new SmsMessage[messages.length];
-        for (int n = 0; n < messages.length; n++) {
-            smsMessage[n] = SmsMessage.createFromPdu((byte[]) messages[n]);
-        }
+        if (intent.getAction().equals(SMS_RECEIVED)) {
 
+            Bundle bundle = intent.getExtras();
+            if(bundle == null) Log.e(TAG, "null bundle");
+            Object[] messages = (Object[]) bundle.get("pdus");
+            smsMessage = new SmsMessage[messages.length];
+            for (int n = 0; n < messages.length; n++) {
+                smsMessage[n] = SmsMessage.createFromPdu((byte[]) messages[n]);
+            }
+            if (messages.length > -1) {
+                Log.i(TAG, "Message recieved: " + smsMessage[0].getMessageBody());
+            }
+        }
         //read each message and send acknowledgement to sender SMS
         SmsManager sms = SmsManager.getDefault();
 
@@ -34,14 +45,14 @@ public class SMSReceiver extends BroadcastReceiver {
 
             //show message body
             Toast.makeText(context, "Received SMS: " + msg.getMessageBody(), Toast.LENGTH_LONG).show();
-            Log.i("SMS","SMS Received");
+            Log.i(TAG,"SMS Received");
 
             String to = msg.getOriginatingAddress();
             Toast.makeText(context, "SMS Originator: " + to, Toast.LENGTH_LONG).show();
-            Log.i("SMS", to);
+            Log.i(TAG, to);
             sms.sendTextMessage(to, null, "Message Acknowledged", null, null);
             Toast.makeText(context, "Reply Sent to " + to, Toast.LENGTH_LONG).show();
-            Log.i("SMS", "SMS Acknowledge Sent");
+            Log.i(TAG, "SMS Acknowledge Sent");
         }
     }
 }
