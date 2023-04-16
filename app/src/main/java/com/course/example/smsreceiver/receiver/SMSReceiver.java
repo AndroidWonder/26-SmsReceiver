c99fc889a75593d8bf831aff7047589750318862
Android Wonder <jim.pepe@verizon.net>
2023-04-16 14:35:44 -0400

    remove deprecated methods

--- app/src/main/java/com/course/example/smsreceiver/receiver/SMSReceiver.java
+++ app/src/main/java/com/course/example/smsreceiver/receiver/SMSReceiver.java
@@ -11,6 +11,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
@@ -31,30 +32,31 @@ public class SMSReceiver extends BroadcastReceiver {
 
             Bundle bundle = intent.getExtras();
             if(bundle == null) Log.e(TAG, "null bundle");
-            Object[] messages = (Object[]) bundle.get("pdus");
-            smsMessage = new SmsMessage[messages.length];
-            for (int n = 0; n < messages.length; n++) {
-                smsMessage[n] = SmsMessage.createFromPdu((byte[]) messages[n]);
+
+            //get messages
+            SmsMessage[] smsMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+
+            //create SmsManager
+            SmsManager sms = SmsManager.getDefault();
+
+            //read each message and send acknowledgement to sender SMS
+            for (SmsMessage message : smsMessages) {
+                //log message body
+                Log.i(TAG, "Message recieved: " + message.getDisplayMessageBody());
+
+                //send acknowledgement to receiver
+                String to = message.getOriginatingAddress();
+                Toast.makeText(context, "SMS Originator: " + to, Toast.LENGTH_LONG).show();
+                Log.i(TAG, to);
+                sms.sendTextMessage(to, null, "Message Acknowledged", null, null);
+                Toast.makeText(context, "Reply Sent to " + to, Toast.LENGTH_LONG).show();
+                Log.i(TAG, "SMS Acknowledge Sent");
             }
-            if (messages.length > -1) {
-                Log.i(TAG, "Message recieved: " + smsMessage[0].getMessageBody());
-            }
+            // End of loop
+
+
+
         }
-        //read each message and send acknowledgement to sender SMS
-        SmsManager sms = SmsManager.getDefault();
 
-        for( SmsMessage msg:smsMessage){
-
-            //show message body
-            Toast.makeText(context, "Received SMS: " + msg.getMessageBody(), Toast.LENGTH_LONG).show();
-            Log.i(TAG,"SMS Received");
-
-            String to = msg.getOriginatingAddress();
-            Toast.makeText(context, "SMS Originator: " + to, Toast.LENGTH_LONG).show();
-            Log.i(TAG, to);
-            sms.sendTextMessage(to, null, "Message Acknowledged", null, null);
-            Toast.makeText(context, "Reply Sent to " + to, Toast.LENGTH_LONG).show();
-            Log.i(TAG, "SMS Acknowledge Sent");
-        }
     }
 }
